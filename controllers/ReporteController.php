1ae3bc62a1e01a9948135f8cc540d40ecc39a257
joanstevanmedinagrisales <joanstevanmedinagrisales@gmail.com>
2026-08-04 07:55:56 -0500

    Se implementan reportes

---
+++ controllers/ReporteController.php
@@ -0,0 +1,14 @@
+<?php
+
+class ReporteController
+{
+    public function generarReporte()
+    {
+        echo "Reporte generado correctamente.";
+    }
+
+    public function mostrarMovimientos()
+    {
+        echo "Listado de movimientos del sistema.";
+    }
+}
